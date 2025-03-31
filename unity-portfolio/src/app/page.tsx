f8d5d9cd62c432164ebf710ad4de16a625ae4c8b
jaehooooooo <funjh002@gmail.com>
2025-04-01 04:11:17 +0900

    fix : 카카오톡 링크오류

--- unity-portfolio/src/app/page.tsx
+++ unity-portfolio/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
                 <title>Ash의 Unity 포트폴리오</title>
                 <meta property="og:title" content="최재호(Ash) Portfolio" />
                 <meta property="og:description" content="인터랙티브 아트와 프로젝트를 확인해보세요." />
-                <meta property="og:image" content="https://ashflow-studio.vercel.app/og-image.png" />
+                <meta property="og:image" content="https://www.ashflow.info/og-image.png" />
                 <meta property="og:url" content="https://ashflow.info" />
                 <meta name="description" content="인터랙티브 아트 개발자 포트폴리오 사이트입니다." />
             </Head>
