5cd87fc45bb5fdeea7cdf12d7c11c0dd8619c182
jaehooooooo <funjh002@gmail.com>
2025-04-01 03:48:08 +0900

    feat : url Project 이름 변경

--- unity-portfolio/src/app/page.tsx
+++ unity-portfolio/src/app/page.tsx
@@ -1,4 +1,5 @@
 //import Image from "next/image";
+import Head from "next/head"; // 요거 꼭 필요!
 import Hero from "@/components/Hero";
 import InfoCard from "@/components/InfoCards"
 import FeatureCards from "@/components/FeatureCards";
@@ -8,6 +9,15 @@ import SuccessGrid from "@/components/SuccessGrid";
 export default function Home() {
     return (
         <main>
+            <Head>
+                <title>Ash의 Unity 포트폴리오</title>
+                <meta property="og:title" content="최재호(Ash) Portfolio" />
+                <meta property="og:description" content="인터랙티브 아트와 프로젝트를 확인해보세요." />
+                <meta property="og:image" content="https://ashflow-studio.vercel.app/og-image.png" />
+                <meta property="og:url" content="https://ashflow.info" />
+                <meta name="description" content="인터랙티브 아트 개발자 포트폴리오 사이트입니다." />
+            </Head>
+
             <Hero />
             <InfoCard/>
             <FeatureCards />
